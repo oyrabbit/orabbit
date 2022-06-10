f3fa486dabfb5306e05623870076250a63029292
lantu <486163408@qq.com>
2022-06-11 01:19:57 +0800

    基础架构

--- src/router/index.js
+++ src/router/index.js
@@ -1,26 +1,34 @@
-import { createRouter, createWebHashHistory } from "vue-router";
-import HomeView from "../views/HomeView.vue";
+import { createRouter, createWebHashHistory } from 'vue-router'
+// import HomeView from "../views/HomeView.vue";
+
+const Layout = () => import('@/views/Layout')
+const Home = () => import('@/views/home/index')
 
 const routes = [
   {
-    path: "/",
-    name: "home",
-    component: HomeView,
+    path: '/',
+    component: Layout,
+    children: [{ path: '/', component: Home }],
   },
-  {
-    path: "/about",
-    name: "about",
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
-  },
-];
+  // {
+  //   path: "/",
+  //   name: "home",
+  //   component: HomeView,
+  // },
+  // {
+  //   path: "/about",
+  //   name: "about",
+  //   // route level code-splitting
+  //   // this generates a separate chunk (about.[hash].js) for this route
+  //   // which is lazy-loaded when the route is visited.
+  //   component: () =>
+  //     import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  // },
+]
 
 const router = createRouter({
   history: createWebHashHistory(),
   routes,
-});
+})
 
-export default router;
+export default router
